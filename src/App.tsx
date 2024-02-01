832772588106c046757600381ebbb369421d7e6d
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-02-01 11:07:09 -0600

    Reusable rect components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const title = "Do Market Research";
+  const id = "BUS-1";
+  const points = 5;
+  return (
+    <div className="px-2 m-2 border rounded-lg bg-gray-50">
+      <div className="py-2 text-base font-semibold font">{title}</div>
+      <div className="flex justify-between gap-4 py-2 text-gray-700">
+        <div>{id}</div>
+        <div>EL</div>
+        <div>{points}</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
