f9922cd9d22a31fcdc92520e20abc65811ce313c
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-02-02 16:00:34 -0600

    40.57

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import TaskCard from "./components/TaskCard";
 
-import { tasks } from "./utils/data-tasks";
+import { tasks, statuses } from "./utils/data-tasks";
 
 function App() {
+  const columns = statuses.map((status) => {
+    const tasksInColumn = tasks.filter((task) => task.status === status);
+    return {
+      title: status,
+      tasks: tasksInColumn,
+    };
+  });
+
   return (
-    <>
-      {tasks.map((task) => (
-        <TaskCard task={task} />
+    <section className="flex divide-y">
+      {columns.map((column) => (
+        <div>
+          <h1>{column.title}</h1>
+          {column.tasks.map((task) => (
+            <TaskCard task={task} />
+          ))}
+        </div>
       ))}
-    </>
+    </section>
   );
 }
 
--- src/utils/data-tasks.tsx
+++ src/utils/data-tasks.tsx
@@ -1,30 +1,64 @@
+export type Status = "todo" | "in-progress" | "done";
+
 export type Task = {
   title: string;
   id: string;
+  status: Status;
   points?: number;
 };
+
+export const statuses: Status[] = ["todo", "in-progress", "done"];
 
 export const tasks: Task[] = [
   {
     title: "Do Market Research",
     id: "BUS-1",
+    status: "done",
     points: 5,
   },
 
   {
     title: "Competitor analysis",
+    status: "done",
     id: "BUS-2",
+    points: 0,
   },
 
   {
     title: "Develop Business Strategy",
     id: "BUS-3",
+    status: "in-progress",
     points: 8,
   },
 
   {
     title: "Develop Marketing Strategy",
     id: "BUS-4",
+    status: "todo",
     points: 5,
+  },
+  {
+    title: "Negotiate partnership",
+    id: "BUS-5",
+    status: "todo",
+    points: 3,
+  },
+  {
+    title: "Develop marketing plan",
+    id: "BUS-6",
+    status: "in-progress",
+    points: 5,
+  },
+  {
+    title: "Implement marketing plan",
+    id: "BUS-7",
+    status: "in-progress",
+    points: 8,
+  },
+  {
+    title: "Evaluate business performance",
+    id: "BUS-8",
+    status: "done",
+    points: 6,
   },
 ];
